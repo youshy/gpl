f45c867163ab1748094afda98212f4b87c1058bc
youshy <devilyoushy@gmail.com>
2019-08-16 11:03:05 +0100

    Hello world!

---
+++ 1-tutorial/1-1-hello-world/helloworld.go
@@ -0,0 +1,9 @@
+package main
+
+// this package handles printing to the console
+import "fmt"
+
+func main() {
+	// Go natively handles Unicode, so there's no problem with printing this out
+	fmt.Println("Hello, 世界!")
+}
