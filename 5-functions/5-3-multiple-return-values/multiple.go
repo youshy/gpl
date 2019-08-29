128b0a9342af8930521dbe315619c28693f263a2
youshy <devilyoushy@gmail.com>
2019-08-29 10:20:36 +0100

    Multiple Return Values + Ex 5.5

--- 5-functions/5-3-multiple-return-values/multiple.go
+++ 5-functions/5-3-multiple-return-values/multiple.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
+	url := "https://" + os.Args[1]
+	words, images, embbedimages, err := CountWordsAndImages(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("url: %s\nwords: %d\nimages: %d\nembbedimages: %d\n", url, words, images, embbedimages)
+}
+
+func finder() {
 	for _, url := range os.Args[1:] {
 		links, err := findLinks(url)
 		if err != nil {
@@ -40,6 +52,59 @@ func findLinks(url string) ([]string, error) {
 	}
 	return visit(nil, doc), nil
 }
+
+// Exercise 5.6
+// countWordsAndImages returns number of words and images
+func CountWordsAndImages(url string) (words, images, embbedimages int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images, embbedimages = countWordsAndImages(doc)
+	return words, images, embbedimages, nil
+}
+
+func countWordsAndImages(n *html.Node) (words, images, embbedimages int) {
+	u := make([]*html.Node, 0) // array of unvisited nodes
+	u = append(u, n)           // append nodes to u
+	for len(u) > 0 {
+		n = u[len(u)-1]
+		u = u[:len(u)-1]
+		switch n.Type {
+		case html.TextNode:
+			words += wordCount(n.Data)
+		case html.ElementNode:
+			if n.Data == "img" {
+				images++
+			}
+			if n.Data == "a" {
+				embbedimages++
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			u = append(u, c)
+		}
+	}
+	return
+}
+
+func wordCount(s string) int {
+	n := 0
+	scan := bufio.NewScanner(strings.NewReader(s))
+	scan.Split(bufio.ScanWords)
+	for scan.Scan() {
+		n++
+	}
+	return n
+}
+
+// /Exercise
 
 func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
