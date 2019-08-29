e896ee3cac7bc26fcdcb961d3cc05cb481fa35d6
youshy <devilyoushy@gmail.com>
2019-08-29 11:36:39 +0100

    Values

---
+++ 5-functions/5-5-function-values/values.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	f := square       // function can be assigned to any variable
+	fmt.Println(f(3)) // "9"
+
+	f = negative
+	fmt.Println(f(3))     // "-3"
+	fmt.Printf("%T\n", f) // "func(int) int"
+
+	/*
+		as this assignmnent will throw compile error
+		it's wrapped in the comment
+
+		f = product // compile error: can't assign f(int, int) int to f(int) int
+	*/
+	fmt.Println(strings.Map(add1, "HAL-9000")) // "IBM.:111"
+	fmt.Println(strings.Map(add1, "VMS"))      // "WNT"
+	fmt.Println(strings.Map(add1, "Admix"))    // "Benjy"
+}
+
+// example functions
+func square(n int) int {
+	return n * n
+}
+
+func negative(n int) int {
+	return -n
+}
+
+func product(m, n int) int {
+	return m * n
+}
+
+// moves every rune + 1
+func add1(r rune) rune {
+	return r + 1
+}
+
+// forEachNode calls the fucntions pre(x) and post(x) for each node
+// x in the tree rooted at n. Both functions are optional
+// pre is called before the children are visited (preorder) and
+// post is called after (postorder)
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+var depth int
+
+// functions that could be used with forEachNode
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
+	}
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		depth--
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+	}
+}
