5c16f6e60876a8b23031732e15501df574c52ef5
youshy <devilyoushy@gmail.com>
2019-09-10 09:29:30 +0100

    Comments on goroutines example

--- 8-goroutines-and-channels/8-1-goroutines/main.go
+++ 8-goroutines-and-channels/8-1-goroutines/main.go
@@ -6,21 +6,30 @@ import (
 )
 
 func main() {
+	// this goroutine runs the spinner function
+	// and then allows for further execution
 	go spinner(100 * time.Millisecond)
 	const n = 45
 	fibN := fib(n) // slow for a reason
 	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
 }
 
+// spinner alterates through range
+// to create simple spinner
 func spinner(delay time.Duration) {
 	for {
 		for _, r := range `-\|/` {
+			// \r is carriage return
+			// returns to the line beginning
+			// without advancing to the new one
 			fmt.Printf("\r%c", r)
 			time.Sleep(delay)
 		}
 	}
 }
 
+// fib is an implementation of fibonacci sequence
+// with a recursive method
 func fib(x int) int {
 	if x < 2 {
 		return x
