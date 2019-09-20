2934583ba35a8087fba7c3725db0bc00a47dcd09
youshy <devilyoushy@gmail.com>
2019-09-20 10:55:25 +0100

    7-3 intro

---
+++ 7-interfaces/7-3-interface-satisfaction/i-cant-get-no-satisfaction.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"time"
+)
+
+func main() {
+}
+
+func satisfingInterface() {
+	var w io.Writer
+	w = os.Stdout         // OK: *os.File has Write method
+	w = new(bytes.Buffer) // OK: *bytes.Buffer has Write method
+	w = time.Second       // compile error: time.Duration lacks Write method
+
+	var rwc io.ReadWriteCloser
+	rwc = os.Stdout         // OK: *os.File has Read, Write, Close methods
+	rwc = new(bytes.Buffer) // compile error: *bytes.Buffer lacks Write method
+}
